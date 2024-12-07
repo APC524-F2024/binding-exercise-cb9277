09f2db914a1e2f88e0bb588a016e57f56e46458e
Callum Boyagoda <80609708+BoyagodaC@users.noreply.github.com>
2024-12-07 16:48:08 -0500

    Add: completed PYBIND11_MODULE

--- src/_core.cpp
+++ src/_core.cpp
@@ -6,6 +6,18 @@
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(_core, m) {
-  /// ... Fill me in!
+PYBIND11_MODULE(_core, m)
+{
+  py::class_<IntegratorBase, std::shared_ptr<IntegratorBase>>(m, "IntegratorBase")
+      .def("f", &IntegratorBase::f)
+      .def("compute_step", &IntegratorBase::compute_step)
+      .def("integrate", &IntegratorBase::integrate);
+
+  py::class_<EulerIntegrator, IntegratorBase, std::shared_ptr<EulerIntegrator>>(m, "EulerIntegrator")
+      .def(py::init<>())
+      .def("compute_step", &EulerIntegrator::compute_step);
+
+  py::class_<HarmonicOscillator, EulerIntegrator, std::shared_ptr<HarmonicOscillator>>(m, "HarmonicOscillator")
+      .def(py::init<>())
+      .def("f", &HarmonicOscillator::f);
 }
