04eeee97ee7f4af830d108b83050edf3629d41cd
Callum Boyagoda <80609708+BoyagodaC@users.noreply.github.com>
2024-12-10 05:41:01 -0500

    test10

--- src/_core.cpp
+++ src/_core.cpp
@@ -21,5 +21,6 @@ PYBIND11_MODULE(_core, m) {
   py::class_<HarmonicOscillator, EulerIntegrator,
              std::shared_ptr<HarmonicOscillator>>(m, "HarmonicOscillator")
       .def(py::init<>())
-      .def("f", &HarmonicOscillator::f);
+      .def("compute_step", &HarmonicOscillator::compute_step)
+      .def("integrate", &HarmonicOscillator::integrate);
 }
