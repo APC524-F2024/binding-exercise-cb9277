edbea3fcfd61b205cedd7436d547dcb0638fa4b1
Callum Boyagoda <80609708+BoyagodaC@users.noreply.github.com>
2024-12-10 05:22:00 -0500

    test4

--- src/integrator.hpp
+++ src/integrator.hpp
@@ -3,8 +3,7 @@
 #include <Eigen/Dense>
 #include <vector>
 
-class IntegratorBase
-{
+class IntegratorBase {
 public:
   virtual Eigen::VectorXd f(double t, const Eigen::VectorXd &y) const = 0;
   virtual ~IntegratorBase() {}
@@ -13,18 +12,15 @@ public:
                                        double h) = 0;
 
   Eigen::MatrixXd integrate(double stepsize, int steps,
-                            const std::vector<double> &init_y)
-  {
+                            const std::vector<double> &init_y) {
     std::size_t order = init_y.size();
 
     Eigen::MatrixXd y(steps, order);
-    for (int i = 0; i < order; i++)
-    {
+    for (int i = 0; i < order; i++) {
       y(0, i) = init_y[i];
     }
 
-    for (int n = 0; n < steps - 1; n++)
-    {
+    for (int n = 0; n < steps - 1; n++) {
       y.row(n + 1) = compute_step(stepsize * n, y.row(n), stepsize);
     }
 
@@ -32,21 +28,17 @@ public:
   };
 };
 
-class EulerIntegrator : public IntegratorBase
-{
+class EulerIntegrator : public IntegratorBase {
 public:
   Eigen::VectorXd compute_step(double t_n, const Eigen::VectorXd &y_n,
-                               double h) override
-  {
+                               double h) override {
     return y_n + h * f(t_n, y_n);
   }
 };
 
-class HarmonicOscillator : public EulerIntegrator
-{
+class HarmonicOscillator : public EulerIntegrator {
 public:
-  Eigen::VectorXd f(double t, const Eigen::VectorXd &y) const override
-  {
+  Eigen::VectorXd f(double t, const Eigen::VectorXd &y) const override {
     Eigen::VectorXd dydt(2);
     dydt[0] = y[1];
     dydt[1] = -y[0];
